f519379e161af4ab2cebda44ed773fe837d224c8
Leandro Marques <Leandro Marques@NOTEBOOK>
2022-09-08 15:32:13 -0300

    AULA 20

--- Pizzaria/src/Utilitarios/Conexao.java
+++ Pizzaria/src/Utilitarios/Conexao.java
@@ -6,6 +6,13 @@
 package Utilitarios;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+//import com.mysql.jdbc.Driver;
+//import org.gjt.mm.mysql.Driver;
 
 /**
  *
@@ -14,14 +21,43 @@ import java.sql.Connection;
 public class Conexao {
 
     private final String URL = "jdbc:mysql://localhost:3306/dbinfoq";
-    private final String Driver = "";
-    private final String Usuario = "";
+  //  private final String Driver = "com.mysql.jdbc.Driver;";
+    private final String Driver = "org.gjt.mm.mysql.Driver";
+    private final String Usuario = "root";
     private final String Senha = "";
-    
-    public Conexao(){
-        
+    private static Connection Con;
+
+    public Conexao() {
+        try {
+
+            Con = DriverManager.getConnection(URL, Usuario, Senha);
+            JOptionPane.showMessageDialog(null, "Conectado com Sucesso", "Conectado", 1);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao Conectar com o Banco", "Erro", 0);
+        }
+
     }
-    public Connection getConnection() {
-        return null;
+
+    public static Connection getConnection() {
+
+        if (Con == null) {
+            new Conexao();
+        }
+        return Con;
+    }
+
+    public static void fecharConexao() {
+        try {
+            if (!Con.isClosed()) {
+                Con.close();
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Conexao.getConnection();
     }
 }
