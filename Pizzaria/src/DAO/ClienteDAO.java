145dd81eede8387d943196326f4c112495605507
Leandro Marques <Leandro Marques@NOTEBOOK>
2022-09-21 19:25:12 -0300

    AULA 27

---
+++ Pizzaria/src/DAO/ClienteDAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Beans.ClienteBeans;
+
+/**
+ *
+ * @author Leandro Marques
+ */
+public class ClienteDAO {
+    
+    public ClienteDAO(){
+        
+    }
+    public void cadastrarCliente(ClienteBeans cliente) {
+        
+        String SQLInsertion = "insert into clientes"
+    }
+}
